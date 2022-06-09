3acd029793795d8c3eb74e64aac1e9119083834a
przemekk6973 <przemek.kukla0703@gmail.com>
2022-06-09 13:44:17 +0200

    User profile added, bugfixes

--- contacts/models.py
+++ contacts/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Contact(models.Model):
+
     full_name = models.CharField(max_length=250)
     relationship = models.CharField(max_length=250)
     email = models.EmailField(max_length=254)
