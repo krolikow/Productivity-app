f6a3b3e18040e17b56fc775eea62f0776562e2b1
przemekk6973 <przemek.kukla0703@gmail.com>
2022-06-09 14:32:19 +0200

    Contacts private

--- contacts/models.py
+++ contacts/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Contact(models.Model):
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="contacts", null=True)
     full_name = models.CharField(max_length=250)
     relationship = models.CharField(max_length=250)
     email = models.EmailField(max_length=254)
